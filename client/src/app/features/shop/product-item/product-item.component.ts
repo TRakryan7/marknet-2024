a591fdebe2a1263d4acc25128fc0a88ccdb7e53b
TRakryan7 <68528468+TRakryan7@users.noreply.github.com>
2024-11-08 10:20:25 +0700

    Akhir section 10

--- client/src/app/features/shop/product-item/product-item.component.ts
+++ client/src/app/features/shop/product-item/product-item.component.ts
@@ -4,6 +4,7 @@ import { MatCardModule } from '@angular/material/card';
 import { CurrencyPipe } from '@angular/common';
 import { MatButton } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-item',
@@ -12,7 +13,8 @@ import { MatIcon } from '@angular/material/icon';
     MatCardModule,
     CurrencyPipe,
     MatButton,
-    MatIcon
+    MatIcon,
+    RouterLink,
   ],
   templateUrl: './product-item.component.html',
   styleUrl: './product-item.component.scss'
